f2cd8cf700e9ca7ac51aafa6e323d418ed1f7bfe
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-10-13 17:02:23 +0300

    added 10

--- Lab1/lab1_ex10.py
+++ Lab1/lab1_ex10.py
@@ -2,3 +2,5 @@
 # form out of words that are separated by only ONE space. For example: "I have Python exam" has 4 words.
 
 # "I have Python exam" -> 4 words
+
+print(len(input("Enter a text and see below how many words exists in it: ").split()))
