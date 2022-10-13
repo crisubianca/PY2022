ad54e372ef7cf2b53978e548573708ca2b68692e
Bianca Crișu <100210311+crisubianca@users.noreply.github.com>
2022-10-13 17:42:05 +0300

    added 9

--- Lab1/lab1_ex9.py
+++ Lab1/lab1_ex9.py
@@ -3,3 +3,20 @@
 # Only letters (A-Z or a-z) are to be considered. Casing should not be considered "A" and "a" represent the same character.
 
 # "an apple is not a tomato" -> the most common is: "a"(4 times)
+from collections import Counter
+
+string = input("Write your string: ")
+
+def commonLetter(text):
+    max = 0
+    letter = ""
+    text = text.lower()
+    for i in text:
+        if "a" <= i <= "z":
+                count = text.count(i)
+                if count > max:
+                    max = count
+                    letter = i
+    print("Common letter is '", letter, "'", "and appears of ", max ,'times!')
+
+commonLetter(string)
